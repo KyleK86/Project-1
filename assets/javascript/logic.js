c97424d9b9ae6c6770ed09207f73d94712950644
pando <spreadpando@gmail.com>
2019-04-27 10:44:59 -0600

    firebase init

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+let config = {
+	apiKey: "AIzaSyA_VBvhCQwkbk792q9BVCv19uC-SAMm8-M",
+	authDomain: "kjw-project-1.firebaseapp.com",
+	databaseURL: "https://kjw-project-1.firebaseio.com",
+	projectId: "kjw-project-1",
+	storageBucket: "kjw-project-1.appspot.com",
+	messagingSenderId: "1048382006751"
+};
+firebase.initializeApp(config);
+let database = firebase.database;
